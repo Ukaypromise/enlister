e1266798610cad448e9694e162dd2f81b5d5d939
Ukaypromise <promiseukaimo@gmail.com>
2023-03-12 12:05:36 +0100

    Setup mongoose and enviromental variables

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,25 @@
-import express from 'express';
+import "dotenv/config";
+import mongoose from "mongoose";
+import express from "express";
+import env from "./util/validateEnv";
 
 const app = express();
-const port =5000;
 
-app.get("/", (req, res)=>{
-    res.send("Hello, World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello, World");
+});
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-    
-})
+const port = env.PORT || 5000;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
